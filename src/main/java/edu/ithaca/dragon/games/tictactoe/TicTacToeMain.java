ad82831399222c311ddde2baf2f7d9292fc756c7
Kemi Odumosu <oodumosu@ithaca.edu>
2021-02-09 12:37:12 -0500

    Made Agent Rule-Based from Expert-Based

--- src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
@@ -2,12 +2,13 @@ package edu.ithaca.dragon.games.tictactoe;
 
 import edu.ithaca.dragon.games.tictactoe.player.BrainDeadPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.HumanPlayer;
-import edu.ithaca.dragon.games.tictactoe.player.KemiPlayer;
-
+import edu.ithaca.dragon.games.tictactoe.player.RuleBasedKemiPlayer;
+import edu.ithaca.dragon.games.tictactoe.player.SRPlayer;
 public class TicTacToeMain {
     public static void main (String[] args){
-        new TicTacToeGame( new KemiPlayer(), new HumanPlayer()).play();
-        //new TicTacToeGame( new BrainDeadPlayer(), new RuleBasedPlayer()).play();
+        // new TicTacToeGame( new RuleBasedKemiPlayer(), new SRPlayer()).play();
+        // new TicTacToeGame( new BrainDeadPlayer(), new RuleBasedKemiPlayer()).play();
+        new TicTacToeGame(new RuleBasedKemiPlayer(), new HumanPlayer()).play();
 
     }
     
